0120ae08d9a68362a1b1bb5793ef553fa8bea8b2
senseiX21 <mahdix2118@protonmail.com>
2023-07-15 20:00:34 +0100

    Updated file to implement RTK query

--- src/page/AllBooks.tsx
+++ src/page/AllBooks.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import { useGetBooksQuery } from '../redux/features/books/bookApi'
 
 export default function AllBooks() {
+    const data = useGetBooksQuery(undefined)
+    console.log(data)
     return (
         <div>AllBooks</div>
     )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { api } from "./api/apiSlice";
 const store = configureStore({
     reducer: {
-
+        [api.reducerPath]: api.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(api.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>;
