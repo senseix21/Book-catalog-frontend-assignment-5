0b3017cc4f7c738e47fa3967467b65632bfc13af
senseiX21 <mahdix2118@protonmail.com>
2023-07-15 14:19:16 +0100

    Added mainLayout

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { Outlet } from 'react-router-dom'
+
+export default function MainLayout() {
+    return (
+        <div>
+            <Navbar />
+            <div className='pt-16'>
+                <Outlet />
+
+            </div>
+        </div>
+    )
+}
