fc3d9a16e951e3e22502261f4fde003579656280
senseiX21 <mahdix2118@protonmail.com>
2023-07-15 19:35:29 +0100

    Created getBook query in book api

---
+++ src/redux/features/books/bookApi.ts
@@ -0,0 +1,13 @@
+import { api } from "../../api/apiSlice";
+
+const bookApi = api.injectEndpoints({
+    endpoints: (builder) => ({
+        getBooks: builder.query({
+            query: () => '/books'
+        })
+    })
+})
+
+export const {
+    useGetBooksQuery
+} = bookApi
